814d8288e0ac13afe8c7f167a773c05b0083b4a7
Gabriel Mongaras <43501738+gmongaras@users.noreply.github.com>
2022-07-08 17:45:31 -0700

    Added double tap to like

--- app/src/main/java/com/example/metau_capstone/Profile/ProfileAdapter.java
+++ app/src/main/java/com/example/metau_capstone/Profile/ProfileAdapter.java
@@ -1,9 +1,13 @@
 package com.example.metau_capstone.Profile;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,19 +15,31 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.metau_capstone.Fortune;
 import com.example.metau_capstone.R;
 import com.example.metau_capstone.dateFormatter;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import Fragments.Main.ProfileDetailFragment;
 
 public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ViewHolder> {
 
+    private final static String TAG = "ProfileAdapter";
+
     // List in the recycler view
     List<Fortune> fortunes;
+
+    // List of liked fortune IDs
+    List<String> likedFortunes;
 
     // Fragment manager for the home fragment
     FragmentManager fragmentManager;
@@ -46,6 +62,19 @@ public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ViewHold
         this.context = context;
         fragmentManager = manager;
         this.mode = mode;
+
+        // Get all liked fortunes from this user
+        ParseRelation<Fortune> likedRel = ParseUser.getCurrentUser().getRelation("liked");
+        ParseQuery<Fortune> query = likedRel.getQuery();
+        query.findInBackground(new FindCallback<Fortune>() {
+            @Override
+            public void done(List<Fortune> objects, ParseException e) {
+                likedFortunes = new ArrayList<>();
+                for (Fortune f : objects) {
+                    likedFortunes.add(f.getObjectId());
+                }
+            }
+        });
     }
 
     @NonNull
@@ -65,23 +94,6 @@ public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ViewHold
 
         // Bind the post to the view holder
         holder.bind(fortune);
-
-        // Put an onClick listener on the view to go into the detailed view
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Setup the fragment switch
-                FragmentTransaction ft = fragmentManager.beginTransaction();
-
-                // Create the fragment with paramters
-                ft.setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
-                ProfileDetailFragment fragmentProfileDetail = ProfileDetailFragment.newInstance(fortune, user, mode);
-
-                // Change the fragment
-                ft.replace(R.id.flContainer, fragmentProfileDetail);
-                ft.commit();
-            }
-        });
     }
 
     @Override
@@ -94,6 +106,19 @@ public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ViewHold
         // Elements in the item view
         TextView tvDate;
         TextView tvFortune;
+        ImageView ivLiked;
+
+        // Used to detect double tapping
+        GestureDetector gestureDetector;
+
+        // Fortune in this view
+        Fortune fortune;
+
+        // Is the fortune liked?
+        boolean liked;
+
+        // Is the fortune changing states?
+        boolean changing;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -101,10 +126,26 @@ public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ViewHold
             // Get the element contents
             tvDate = itemView.findViewById(R.id.tvDate);
             tvFortune = itemView.findViewById(R.id.tvFortune);
+            ivLiked = itemView.findViewById(R.id.ivLiked);
+
+            // Create a new gesture detector to handle double taps
+            gestureDetector = new GestureDetector(itemView.getContext(), new GestureListener());
+
+            // Handle double taps on the entire view
+            itemView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    itemView.performClick();
+                    return gestureDetector.onTouchEvent(event);
+                }
+            });
         }
 
         // Given a fortune, bind data to this object
         public void bind(Fortune fortune) {
+            this.fortune = fortune;
+            changing = false;
+
             // Get the date in the proper form and set it
             tvDate.setText(df.toMonthDay(fortune.getCreatedAt()));
 
@@ -113,6 +154,178 @@ public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ViewHold
                 message = message.substring(0, 50) + "...";
             }
             tvFortune.setText(message);
+
+
+            // If the liked list is null, get the list
+            if (likedFortunes == null) {
+                changing = true;
+                // Get the user's liked fortunes
+                ParseRelation<Fortune> likedRel = ParseUser.getCurrentUser().getRelation("liked");
+                ParseQuery<Fortune> query = likedRel.getQuery();
+                query.whereEqualTo("objectId", fortune.getObjectId());
+                query.findInBackground(new FindCallback<Fortune>() {
+                    @Override
+                    public void done(List<Fortune> objects, ParseException e) {
+                        // If the objects returned is 0, the fortune is not liked
+                        if (objects.size() == 0) {
+                            liked = false;
+                        } else {
+                            liked = true;
+                        }
+
+                        // Change the drawable based on the liked state
+                        if (liked) {
+                            Glide.with(itemView.getContext())
+                                    .load(R.drawable.like_filled)
+                                    .circleCrop()
+                                    .into(ivLiked);
+                        } else {
+                            Glide.with(itemView.getContext())
+                                    .load(R.drawable.like)
+                                    .circleCrop()
+                                    .into(ivLiked);
+                        }
+
+                        changing = false;
+                    }
+                });
+            }
+
+            // If the liked list is loaded, load in the image
+            else {
+
+                changing = true;
+
+                // Does the user like this fortune
+                if (likedFortunes.contains(fortune.getObjectId())) {
+                    liked = true;
+                } else {
+                    liked = false;
+                }
+
+                // Change the drawable based on the liked state
+                if (liked) {
+                    Glide.with(itemView.getContext())
+                            .load(R.drawable.like_filled)
+                            .circleCrop()
+                            .into(ivLiked);
+                } else {
+                    Glide.with(itemView.getContext())
+                            .load(R.drawable.like)
+                            .circleCrop()
+                            .into(ivLiked);
+                }
+
+                changing = false;
+            }
+        }
+
+        // Clicks on the view
+        private class GestureListener extends GestureDetector.SimpleOnGestureListener {
+            // Detect clicks on the view
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+
+            // Like the fortune if a double tap occurs
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                // If the fortune is already being liked, do nothing
+                if (changing) {
+                    return true;
+                }
+
+                changing = true;
+                // If the item is already liked, unlike it
+                if (liked) {
+                    // Update the backend
+                    ParseRelation<Fortune> likedRel = user.getRelation("liked");
+                    likedRel.remove(fortune);
+                    user.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e != null) {
+                                Log.e(TAG, "Error saving unlike", e);
+                            }
+                        }
+                    });
+                    fortune.put("like_ct", fortune.getInt("like_ct")-1);
+                    fortune.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e != null) {
+                                Log.e(TAG, "Error saving like count", e);
+                            }
+                            changing = false;
+                        }
+                    });
+
+                    // Update the stored states
+                    liked = false;
+                    likedFortunes.remove(fortune.getObjectId());
+
+                    // Update the image;
+                    Glide.with(itemView.getContext())
+                            .load(R.drawable.like)
+                            .circleCrop()
+                            .into(ivLiked);
+                }
+                // If the item is not liked, like it
+                else {
+                    // Update the backend
+                    ParseRelation<Fortune> likedRel = user.getRelation("liked");
+                    likedRel.add(fortune);
+                    user.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e != null) {
+                                Log.e(TAG, "Error saving like", e);
+                            }
+                        }
+                    });
+                    fortune.put("like_ct", fortune.getInt("like_ct")+1);
+                    fortune.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e != null) {
+                                Log.e(TAG, "Error saving like count", e);
+                            }
+                            changing = false;
+                        }
+                    });
+
+                    // Update the stored states
+                    liked = true;
+                    likedFortunes.add(fortune.getObjectId());
+
+                    // Update the image;
+                    Glide.with(itemView.getContext())
+                            .load(R.drawable.like_filled)
+                            .circleCrop()
+                            .into(ivLiked);
+                }
+
+
+                return true;
+            }
+
+            // Go into the detailed view when a single tap occurs
+            @Override
+            public boolean onSingleTapConfirmed(MotionEvent e) {
+                // Setup the fragment switch
+                FragmentTransaction ft = fragmentManager.beginTransaction();
+
+                // Create the fragment with paramters
+                ft.setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
+                ProfileDetailFragment fragmentProfileDetail = ProfileDetailFragment.newInstance(fortune, user, mode);
+
+                // Change the fragment
+                ft.replace(R.id.flContainer, fragmentProfileDetail);
+                ft.commit();
+
+                return super.onSingleTapConfirmed(e);
+            }
         }
     }
 }
